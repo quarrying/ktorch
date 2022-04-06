4640bf043bbad14d70feb3a2de3beca7e1adff00
quarrying <quarrying@qq.com>
2022-04-06 23:11:31 +0800

    add GpBnFcBn

--- ktorch/models/others.py
+++ ktorch/models/others.py
@@ -1,9 +1,10 @@
 import torch
+import torch.nn as nn
 
-__all__ = ['ClassifierModel']
+__all__ = ['ClassifierModel', 'GpBnFcBn']
 
 
-class ClassifierModel(torch.nn.Module):
+class ClassifierModel(nn.Module):
     def __init__(self, backbone, head):
         super(ClassifierModel, self).__init__()
         self.backbone = backbone
@@ -17,3 +18,26 @@ class ClassifierModel(torch.nn.Module):
             x = self.head(x, label)
         return x
         
+
+class GpBnFcBn(nn.Module):
+    def __init__(self, in_channels, embedding_size=512, pooling_type='gap'):
+        super(GpBnFcBn, self).__init__()
+        if pooling_type.lower() == 'gmp':
+            self.gp = nn.AdaptiveMaxPool2d(1)
+        elif pooling_type.lower() == 'gap':
+            self.gp = nn.AdaptiveAvgPool2d(1)
+        else:
+            raise ValueError('Unsupported pooling_type!')
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.bn1.bias.requires_grad_(False)
+        self.fc = nn.Conv2d(in_channels, embedding_size, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(embedding_size)
+        self.bn2.bias.requires_grad_(False)
+ 
+    def forward(self, x):
+        x = self.gp(x)
+        x = self.bn1(x)
+        x = self.fc(x)
+        x = self.bn2(x)
+        return x
+
