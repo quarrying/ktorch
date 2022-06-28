b861a76cfe7ed3cae2eba4dcfc278d71abe8c1c6
quarrying <quarrying@qq.com>
2022-06-28 23:38:17 +0800

    fix a bug in PathAndImageDataset

--- ktorch/datasets/common.py
+++ ktorch/datasets/common.py
@@ -46,12 +46,17 @@ class PathImageAndLabelDataset(torch.utils.data.Dataset):
 
 class PathAndImageDataset(torch.utils.data.Dataset):
     def __init__(self, filename, transform=None):
-        if os.path.isfile(filename):
-            self.records = khandy.load_list(filename)
-        elif os.path.isdir(filename):
-            self.records = khandy.get_all_filenames(filename)
+        if isinstance(filename, str):
+            if os.path.isfile(filename):
+                self.records = khandy.load_list(filename)
+            elif os.path.isdir(filename):
+                self.records = khandy.get_all_filenames(filename)
+            else:
+                raise ValueError('file or dir does not exist!')
         elif isinstance(filename, (list, tuple)):
             self.records = filename
+        else:
+            raise ValueError('unsupported type!')
         self.transform = transform
 
     def __len__(self):
