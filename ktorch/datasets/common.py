e3736d2203674a92a10a6f92d3bebd3475766f68
quarrying <quarrying@qq.com>
2022-07-21 20:28:28 +0800

    update khandy.imread_pil according to khandy, and some other fixes

--- ktorch/datasets/common.py
+++ ktorch/datasets/common.py
@@ -19,8 +19,7 @@ class ImageAndLabelDataset(torch.utils.data.Dataset):
         parts = record.split(',')
         path = ','.join(parts[:-1])
         label = int(parts[-1])
-        image = khandy.imread_pil(path)
-        image = image.convert('RGB')
+        image = khandy.imread_pil(path, 'RGB')
         if self.transform:
             image = self.transform(image)
         return image, label
@@ -39,35 +38,33 @@ class PathImageAndLabelDataset(torch.utils.data.Dataset):
         parts = record.split(',')
         path = ','.join(parts[:-1])
         label = int(parts[-1])
-        image = khandy.imread_pil(path)
-        image = image.convert('RGB')
+        image = khandy.imread_pil(path, 'RGB')
         if self.transform:
             image = self.transform(image)
         return path, image, label
 
 
 class PathAndImageDataset(torch.utils.data.Dataset):
-    def __init__(self, filename, transform=None):
-        if isinstance(filename, str):
-            if os.path.isfile(filename):
-                self.records = khandy.load_list(filename)
-            elif os.path.isdir(filename):
-                self.records = khandy.get_all_filenames(filename)
+    def __init__(self, filename_or_else, transform=None):
+        if isinstance(filename_or_else, str):
+            if os.path.isfile(filename_or_else):
+                self.paths = khandy.load_list(filename_or_else)
+            elif os.path.isdir(filename_or_else):
+                self.paths = khandy.get_all_filenames(filename_or_else)
             else:
                 raise ValueError('file or dir does not exist!')
-        elif isinstance(filename, (list, tuple)):
-            self.records = filename
+        elif isinstance(filename_or_else, (list, tuple)):
+            self.paths = filename_or_else
         else:
             raise ValueError('unsupported type!')
         self.transform = transform
 
     def __len__(self):
-        return len(self.records)
+        return len(self.paths)
     
     def __getitem__(self, idx):
-        path = self.records[idx]
-        image = khandy.imread_pil(path)
-        image = image.convert('RGB')
+        path = self.paths[idx]
+        image = khandy.imread_pil(path, 'RGB')
         if self.transform:
             image = self.transform(image)
         return path, image
