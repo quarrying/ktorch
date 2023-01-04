5e79794fa02436b28a762077dcdd99b0e61595bc
quarryman <quarrying@qq.com>
2023-01-04 12:09:54 +0000

    add CBAM

--- ktorch/models/attention.py
+++ ktorch/models/attention.py
@@ -1,6 +1,7 @@
+import torch
 import torch.nn as nn
 
-__all__ = ['SEBlock']
+__all__ = ['SEBlock', 'CBAM']
 
 
 class SEBlock(nn.Module):
@@ -19,4 +20,62 @@ class SEBlock(nn.Module):
         y = self.gap(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
         return x * y
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1   = nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+    
+    
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size % 2 == 1, "Odd kernel size required"
+        self.conv = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=kernel_size,
+                              padding=int((kernel_size - 1) / 2), bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # (global) channel pooling, dim=1 is channel
+        # (B, 1, H, W)
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        # (B, 1, H, W)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        # (B, 2, H, W)
+        x = torch.cat([avg_out, max_out], dim=1)
+        # (B, 1, H, W)
+        x = self.conv(x)
+        return self.sigmoid(x)
+
+
+class CBAM(nn.Module):
+    def __init__(self, in_channels, reduction_ratio, kernel_size):
+        super(CBAM, self).__init__()
+        self.in_channels = in_channels
+        self.reduction_ratio = reduction_ratio
+        self.kernel_size = kernel_size
+
+        self.channel_attention = ChannelAttention(in_channels, reduction_ratio)
+        self.spatial_attention = SpatialAttention(kernel_size)
+
+    def forward(self, f):
+        chan_att = self.channel_attention(f)
+        fp = chan_att * f
+        spat_att = self.spatial_attention(fp)
+        fpp = spat_att * fp
+        return fpp
 
