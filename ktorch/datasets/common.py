f8302f4770d08652cd095f6b96b8ab04493e21c2
quarryman <quarrying@qq.com>
2023-04-05 20:14:09 +0800

    refactor ImageAndLabelDataset and add RecordDataset

--- ktorch/datasets/common.py
+++ ktorch/datasets/common.py
@@ -3,31 +3,44 @@ import os
 import torch
 import khandy
 
-__all__ = ['ImageAndLabelDataset', 'PathImageAndLabelDataset', 'PathAndImageDataset']
+__all__ = ['RecordDataset', 'ImageAndLabelDataset', 'PathImageAndLabelDataset', 'PathAndImageDataset']
 
+
+class RecordDataset(torch.utils.data.Dataset):
+    def __init__(self, filename, transform=None):
+        self.records = khandy.load_list(filename)
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.records)
+    
+    def __getitem__(self, idx):
+        record = self.records[idx]
+        return self.transform(record)
+    
 
 class ImageAndLabelDataset(torch.utils.data.Dataset):
-    def __init__(self, filename_or_else, transform=None):
+    def __init__(self, filename_or_else, transform=None, label_transform=int):
         if isinstance(filename_or_else, str):
             self.records = khandy.load_list(filename_or_else)
         elif isinstance(filename_or_else, (list, tuple)):
             self.records = filename_or_else
         else:
             raise ValueError('unsupported type!')
-        
         self.transform = transform
+        self.label_transform = label_transform
 
     def __len__(self):
         return len(self.records)
     
     def __getitem__(self, idx):
         record = self.records[idx]
-        parts = record.split(',')
-        path = ','.join(parts[:-1])
-        label = int(parts[-1])
+        path, label = record.rsplit(',', maxsplit=1)
         image = khandy.imread_pil(path, 'RGB')
         if self.transform:
             image = self.transform(image)
+        if self.label_transform is not None:
+            label = self.label_transform(label)
         return image, label
 
 
