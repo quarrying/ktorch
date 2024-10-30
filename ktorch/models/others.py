ca32e6fc754eb0fd8e1b355838b4bc42a1ce039b
quarrying <quarrying@qq.com>
2024-10-30 19:44:23 +0800

    add CCLNorm2d

--- ktorch/models/others.py
+++ ktorch/models/others.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 from .pooling import GlobalAttentionPooling
 
-__all__ = ['ClassifierModel', 'GpBnFcBn', 'GpLnFcLn']
+__all__ = ['ClassifierModel', 'GpBnFcBn', 'GpLnFcLn', 'CCLNorm2d']
 
 
 class ClassifierModel(nn.Module):
@@ -73,3 +73,67 @@ class GpLnFcLn(nn.Module):
         return x
     
     
+class CCLNorm2d(torch.nn.BatchNorm2d):
+    """
+    References:
+        [2018] Face Recognition via Centralized Coordinate Learning
+    """
+    def __init__(self,
+        num_features: int,
+        eps: float = 1e-5,
+        momentum: float = 0.1,
+        track_running_stats: bool = True,
+        device=None,
+        dtype=None
+    ) -> None:
+        super().__init__(num_features, eps, momentum, False, track_running_stats, device, dtype)
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        self.weight_scalar = torch.nn.Parameter(torch.empty(1, **factory_kwargs))
+        
+    def reset_parameters(self) -> None:
+        self.reset_running_stats()
+        torch.nn.init.ones_(self.weight)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        self._check_input_dim(input)
+
+        # exponential_average_factor is set to self.momentum
+        # (when it is available) only so that it gets updated
+        # in ONNX graph when this node is exported to ONNX.
+        if self.momentum is None:
+            exponential_average_factor = 0.0
+        else:
+            exponential_average_factor = self.momentum
+
+        if self.training and self.track_running_stats:
+            # TODO: if statement only here to tell the jit to skip emitting this when it is None
+            if self.num_batches_tracked is not None:  # type: ignore[has-type]
+                self.num_batches_tracked.add_(1)  # type: ignore[has-type]
+                if self.momentum is None:  # use cumulative moving average
+                    exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
+
+        # Decide whether the mini-batch stats should be used for normalization rather than the buffers.
+        # Mini-batch stats are used in training mode, and in eval mode when buffers are None.
+        if self.training:
+            bn_training = True
+        else:
+            bn_training = (self.running_mean is None) and (self.running_var is None)
+
+        # Buffers are only updated if they are to be tracked and we are in training mode. Thus they only need to be
+        # passed when the update should occur (i.e. in training mode when they are tracked), or when buffer stats are
+        # used for normalization (i.e. in eval mode when buffers are not None).
+        weight_vector = torch.full_like((self.num_features,), self.weight_scalar.item())
+        return torch.nn.functional.batch_norm(
+            input,
+            # If buffers are not to be tracked, ensure that they won't be updated
+            self.running_mean if not self.training or self.track_running_stats else None,
+            self.running_var if not self.training or self.track_running_stats else None,
+            weight_vector,
+            self.bias,
+            bn_training,
+            exponential_average_factor,
+            self.eps,
+        )
+
