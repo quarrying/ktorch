2d92249ea5bb6240636059725befc8e76c859cc0
quarrying <quarrying@qq.com>
2024-10-30 19:42:51 +0800

    add GlobalAttentionPooling and GpLnFcLn, refactor GpBnFcBn

--- ktorch/models/others.py
+++ ktorch/models/others.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
 
-__all__ = ['ClassifierModel', 'GpBnFcBn']
+from .pooling import GlobalAttentionPooling
+
+__all__ = ['ClassifierModel', 'GpBnFcBn', 'GpLnFcLn']
 
 
 class ClassifierModel(nn.Module):
@@ -26,6 +28,8 @@ class GpBnFcBn(nn.Module):
             self.gp = nn.AdaptiveMaxPool2d(1)
         elif pooling_type.lower() == 'gap':
             self.gp = nn.AdaptiveAvgPool2d(1)
+        elif pooling_type.lower() in ['global_attention_pooling', 'gp_attn']:
+            self.gp = GlobalAttentionPooling()
         else:
             raise ValueError('Unsupported pooling_type!')
         self.bn1 = nn.BatchNorm2d(in_channels)
@@ -41,3 +45,31 @@ class GpBnFcBn(nn.Module):
         x = self.bn2(x)
         return x
 
+
+class GpLnFcLn(nn.Module):
+    def __init__(self, in_channels, embedding_size=512, pooling_type='gap'):
+        super(GpLnFcLn, self).__init__()
+        if pooling_type.lower() == 'gmp':
+            self.gp = nn.AdaptiveMaxPool2d(1)
+        elif pooling_type.lower() == 'gap':
+            self.gp = nn.AdaptiveAvgPool2d(1)
+        elif pooling_type.lower() == 'global_attention_pooling':
+            self.gp = GlobalAttentionPooling()
+        else:
+            raise ValueError('Unsupported pooling_type!')
+        self.ln1 = nn.LayerNorm((in_channels, 1, 1))
+        self.ln1.bias.requires_grad_(False)
+        self.fc = nn.Conv2d(in_channels, embedding_size, 1, bias=False)
+        self.ln2 = nn.LayerNorm((in_channels, 1, 1))
+        self.ln2.bias.requires_grad_(False)
+        
+    def forward(self, x):
+        x = self.gp(x)
+        x = self.ln1(x)
+        # 这里不用残差连接, 是因为 fc 层的输入输出通道数不匹配
+        # 因为没有用残差连接, 所以没有 PreNorm 和 PostNorm 之分
+        x = self.fc(x)
+        x = self.ln2(x)
+        return x
+    
+    
--- ktorch/models/pooling.py
+++ ktorch/models/pooling.py
@@ -1,6 +1,7 @@
 import torch
 
-__all__ = ['GMAP', 'MixedPool2d', 'AdaptiveMixedPool2d']
+__all__ = ['GMAP', 'MixedPool2d', 'AdaptiveMixedPool2d', 
+           'global_attention_pooling', 'GlobalAttentionPooling']
 
 
 class GMAP(torch.nn.Module):
@@ -63,3 +64,28 @@ class AdaptiveMixedPool2d(torch.nn.Module):
         weight = torch.sigmoid(self.weight)
         return y_max + weight * (y_avg - y_max)
         
+        
+def global_attention_pooling(x, scale=1, keepdim=True):
+    assert x.ndim >= 3
+    # (..., C, H * W)
+    x = x.flatten(-2)
+    # (..., C, H * W)
+    attn = torch.softmax(x * scale, dim=-1)
+    # (..., C)
+    attn_gp = torch.sum(x * attn, dim=-1)
+
+    if keepdim:
+        # (..., C, 1, 1)
+        attn_gp = attn_gp.unsqueeze(-1).unsqueeze(-1)
+    return attn_gp
+
+
+class GlobalAttentionPooling(torch.nn.Module):
+    def __init__(self, scale=1):
+        super(GlobalAttentionPooling, self).__init__()
+        self.scale = scale
+        
+    def forward(self, x):
+        return global_attention_pooling(x, self.scale, keepdim=True)
+    
+    
