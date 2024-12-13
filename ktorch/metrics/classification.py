a1f30364547b0989c2e805ac4a8f148797835311
quarryman <quarrying@qq.com>
2024-12-13 20:08:36 +0800

    add ClasswiseTopK

--- ktorch/metrics/classification.py
+++ ktorch/metrics/classification.py
@@ -1,6 +1,6 @@
 import torch
 
-__all__ = ['accuracy', 'TopK', 'ConfusionMatrix']
+__all__ = ['accuracy', 'TopK', 'ConfusionMatrix', 'ClasswiseTopK']
 
 
 def accuracy(output, target, topk=(1,)):
@@ -82,3 +82,37 @@ class ConfusionMatrix(object):
         torch.distributed.barrier()
         torch.distributed.all_reduce(self.mat)
 
+
+class ClasswiseTopK(object):
+    def __init__(self, num_classes, top_k):
+        self.num_classes = num_classes
+        self.top_k = top_k
+        self.max_k = max(self.top_k)
+        self.num_corrects = None
+        self.num_examples = None
+        
+    def update(self, output, target):
+        if self.num_corrects is None:
+            self.num_corrects = torch.zeros((self.num_classes, len(self.top_k)), dtype=torch.int64, device=output.device)
+        if self.num_examples is None:
+            self.num_examples = torch.zeros((self.num_classes,), dtype=torch.int64, device=output.device)
+        
+        with torch.no_grad():
+            max_k = min(self.max_k, output.shape[1])
+            _, pred = torch.topk(output, max_k, 1, True, True)
+            correct = pred.eq(target[:, None])
+            batch_size = len(target)
+            for batch_ind in range(batch_size):
+                class_ind = target[batch_ind]
+                one_correct = correct[batch_ind]
+                for top_ind, k in enumerate(self.top_k):
+                    self.num_corrects[class_ind, top_ind] += torch.sum(one_correct[:k], dtype=torch.int64)
+            self.num_examples += torch.bincount(target, minlength=self.num_classes)
+
+    def reset(self):
+        self.num_corrects.zero_()
+        self.num_examples.zero_()
+
+    def compute(self):
+        return self.num_corrects.float() / self.num_examples[:, None]
+    
