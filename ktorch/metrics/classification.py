03fb6b906b173a19b659265d2f2bb6b66b269ecf
quarryman <quarrying@qq.com>
2024-12-13 23:51:38 +0800

    some minor fixes in metrics

--- ktorch/metrics/classification.py
+++ ktorch/metrics/classification.py
@@ -3,58 +3,61 @@ import torch
 __all__ = ['accuracy', 'TopK', 'ConfusionMatrix', 'ClasswiseTopK']
 
 
-def accuracy(output, target, topk=(1,)):
+def accuracy(output, target, top_k=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k
     
     References:
         torchvision
     """
     with torch.no_grad():
-        maxk = max(topk)
+        max_k = max(top_k)
         batch_size = target.size(0)
 
-        _, pred = output.topk(maxk, 1, True, True)
-        # (batch_size, maxk) -> (maxk, batch_size)
+        _, pred = output.topk(max_k, 1, True, True)
+        # (batch_size, max_k) -> (max_k, batch_size)
         pred = pred.t()
-        # (maxk, batch_size) == (1, batch_size) -> (maxk, batch_size)
+        # (max_k, batch_size) == (1, batch_size) -> (max_k, batch_size)
         correct = pred.eq(target[None])
 
         res = []
-        for k in topk:
+        for k in top_k:
             correct_k = correct[:k].flatten().sum(dtype=torch.float32)
             res.append(correct_k * (100.0 / batch_size))
         return res
 
 
 class TopK(object):
-    def __init__(self, topk):
-        self.topk = topk
-        self.maxk = max(self.topk)
-        self.correct_k = None
+    def __init__(self, top_k):
+        self.top_k = top_k
+        self.max_k = max(self.top_k)
+        self.num_corrects = None
         self.num_examples = 0
         
     def update(self, output, target):
-        if self.correct_k is None:
-            self.correct_k = torch.zeros((len(self.topk),), dtype=torch.int64, device=output.device)
+        if self.num_corrects is None:
+            self.num_corrects = torch.zeros((len(self.top_k),), dtype=torch.int64, device=output.device)
         with torch.no_grad():
-            maxk = min(self.maxk, output.shape[1])
-            _, pred = torch.topk(output, maxk, 1, True, True)
+            max_k = min(self.max_k, output.shape[1])
+            _, pred = torch.topk(output, max_k, 1, True, True)
             pred = pred.t()
+            # pred:            (max_k, batch_size)
+            # target[:, None]: (1, batch_size)
+            # correct:         (max_k, batch_size)
             correct = pred.eq(target[None])
-            for ind, k in enumerate(self.topk):
-                self.correct_k[ind] += torch.sum(correct[:k], dtype=torch.int64)
+            for top_ind, k in enumerate(self.top_k):
+                self.num_corrects[top_ind] += torch.sum(correct[:k], dtype=torch.int64)
         self.num_examples += len(output)
         
     def reset(self):
-        self.correct_k.zero_()
+        self.num_corrects.zero_()
         self.num_examples = 0
 
     def compute(self):
-        return self.correct_k.float() / self.num_examples
+        return self.num_corrects.float() / self.num_examples
         
 
 class ConfusionMatrix(object):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes: int):
         self.num_classes = num_classes
         self.mat = None
 
@@ -84,7 +87,7 @@ class ConfusionMatrix(object):
 
 
 class ClasswiseTopK(object):
-    def __init__(self, num_classes, top_k):
+    def __init__(self, num_classes: int, top_k):
         self.num_classes = num_classes
         self.top_k = top_k
         self.max_k = max(self.top_k)
@@ -100,6 +103,9 @@ class ClasswiseTopK(object):
         with torch.no_grad():
             max_k = min(self.max_k, output.shape[1])
             _, pred = torch.topk(output, max_k, 1, True, True)
+            # pred:            (batch_size, max_k)
+            # target[:, None]: (batch_size, 1)
+            # correct:         (batch_size, max_k)
             correct = pred.eq(target[:, None])
             batch_size = len(target)
             for batch_ind in range(batch_size):
